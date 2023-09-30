bbc0e9bc0f26717dd0b3d5db496f6285b89d76bb
fs2022 <fabiolfs761@gmail.com>
2023-09-30 20:38:44 -0300

    Criada a Header Component para o menu e a frontend para exibir o component

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,27 +1,25 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link } from "@inertiajs/vue3";
+import Frontend from "@/Layouts/Frontend.vue";
+// import Hero from "resources/js/Components/Frontend/Hero.vue";
+// import Promote from "resources/js/Components/Frontend/Promote.vue";
+// import About from "resources/js/Components/Frontend/About.vue";
+// import Skills from "resources/js/Components/Frontend/Skills.vue";
+// import Portfolio from "resources/js/Components/Frontend/Portfolio.vue";
+// import Services from "resources/js/Components/Frontend/Services.vue";
+// import ContactMe from "../Components/Frontend/ContactMe.vue";
 
-defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
-});
+// defineProps({
+//   skills: Object,
+//   projects: Object,
+// });
 </script>
 
 <template>
-    <Head title="Planb WebApp" />
-    <h1>Home Portfólio</h1>
+  <Head title="Welcome to portfolio" />
+  <Frontend>
+ 
+  </Frontend>
 </template>
     
 
