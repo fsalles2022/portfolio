e2c798907a4549e6741ebbcf76108c26eec89495
fs2022 <fabiolfs761@gmail.com>
2023-10-01 14:45:45 -0300

    Implementando componente Footer e importando na Frontend.vue

--- resources/js/Layouts/Frontend.vue
+++ resources/js/Layouts/Frontend.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Header from "@/Components/Frontend/Header.vue";
-// import Footer from "@/Components/Frontend/Footer.vue";
+import Footer from "@/Components/Frontend/Footer.vue";
 </script>
 <template>
   <div class="bg-slate-200 dark:bg-slate-900">
@@ -10,6 +10,6 @@ import Header from "@/Components/Frontend/Header.vue";
       <slot />
     </main>
     <!-- Footer -->
-    <!-- <Footer /> -->
+    <Footer /> 
   </div>
 </template>
