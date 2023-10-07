3b9b004b45dd3775189ea73aab84a3aeed6161c1
fs2022 <fabiolfs761@gmail.com>
2023-10-06 23:25:34 -0300

    Adicionando MotionPlugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,15 @@
 import './bootstrap';
 import '../css/app.css';
+import { MotionPlugin } from '@vueuse/motion';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+
+const appName = import.meta.env.VITE_APP_NAME || 'Portfolio';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,7 +18,9 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(MotionPlugin)
             .mount(el);
+           
     },
     progress: {
         color: '#4B5563',
